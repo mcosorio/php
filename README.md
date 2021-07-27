ab09b6c8419df0534f2b97b7b7b221996224dc10
Mario Osorio <mario.neto@trt12.jus.br>
2021-07-26 22:40:01 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,9 @@
+## Clonar o repositório
+
+    git clone ...
+
+## Para subir
+
+Entrar na pasta `php` e digitar:
+
+    docker-compose up
---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+mysqli_connect("db", "root", "phprs") or die(mysqli_error());
+echo "Connected to MySQL<br />";
+?>
+
+<?php
+phpinfo();
+?>
+
